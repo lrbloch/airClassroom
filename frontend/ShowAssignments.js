640f2c357716be0006ad4a067dc850aa7b3cc155
Laura Bloch <lbloch220@gmail.com>
2020-07-03 13:22:09 -0400

    some refactoring, added upcoming assignments

--- frontend/ShowAssignments.js
+++ frontend/ShowAssignments.js
@@ -3,6 +3,13 @@ import { Box, Heading, expandRecord, Icon } from "@airtable/blocks/ui";
 import moment from 'moment';
 moment().format();
 
+/** @enum {string} */
+export const assignmentDueTypes = {
+  OVERDUE: "Overdue",
+  TODAY: "Due Today",
+  UPCOMING: "Upcoming"
+}
+
 function ShowIndividualAssignment({record, onClick}){
   return (
     <Fragment>
@@ -22,12 +29,9 @@ function ShowIndividualAssignment({record, onClick}){
   );
 }
 
-function OverDueAssignments({records, onClick, toggleShow, showHide}){
-  var now = moment();
-  var filteredRecords = _.filter(records, function(record){
-    return moment(record.getCellValue("Due")).isBefore(now, 'day');
-  });
-  const recordsDisplay = filteredRecords.length > 0 ? filteredRecords.map((record, index) => {
+function OverDueAssignments({records, onClick, toggleShow, showHide, assignmentDue}){
+  var filteredRecords = filterRecords(records, assignmentDue);
+  const recordsDisplay = (filteredRecords !=null && filteredRecords.length > 0) ? filteredRecords.map((record, index) => {
       return (
         <Box
           fontSize={4}
@@ -57,9 +61,9 @@ function OverDueAssignments({records, onClick, toggleShow, showHide}){
           <a
           style={{cursor: 'pointer', flex: 'auto', padding: 8}}
           onClick={() => {
-            toggleShow('overdue');
+            toggleShow(assignmentDue);
           }}>
-            <Heading>Overdue ({recordsDisplay ? recordsDisplay.length : "0"})
+            <Heading> {assignmentDue} ({recordsDisplay ? recordsDisplay.length : "0"})
               <Icon name={showHide ? "chevronUp" : "chevronDown"} size={20}/>
             </Heading> 
           </a>
@@ -77,55 +81,25 @@ function OverDueAssignments({records, onClick, toggleShow, showHide}){
     );
 }
 
-function DueTodayAssignments({records, onClick, toggleShow, showHide}){
-  var filteredRecords = _.filter(records, function(record){
-    return moment(record.getCellValue("Due")).isSame(moment(), 'day');
-  });
-  const recordsDisplay = filteredRecords.length > 0 ? filteredRecords.map((record, index) => {
-      return (
-        <Box
-          fontSize={4}
-          paddingX={3}
-          paddingY={2}
-          marginRight={-2}
-          borderBottom="default"
-          display="flex"
-          alignItems="center"
-          overflowY="auto"
-          key={index}
-      ><a
-        style={{cursor: 'pointer', flex: 'auto', padding: 8}}
-        onClick={() => {
-            onClick(record);
-        }}
-        >
-        {moment(record.getCellValue("Due")).format("LT")}
-      </a></Box>)
-    }) : null;
-
-    return (
-      <Fragment>
-        <br></br>
-        <Box>
-          <a
-          style={{cursor: 'pointer', flex: 'auto', padding: 8}}
-          onClick={() => {
-            toggleShow('today');
-          }}>
-            <Heading>Due Today ({recordsDisplay ? recordsDisplay.length : "0"})
-              <Icon name={showHide ? "chevronUp" : "chevronDown"} size={20}/>
-            </Heading> 
-          </a>
-        </Box>
-        {showHide? (
-        <Box margin={2} padding={3} border="thick" borderRadius={5} overflow="auto">
-            <Box overflow="auto" paddingRight={3}>
-                {recordsDisplay}
-            </Box>
-        </Box>
-        ):(<></>)}
-      </Fragment>
-    );
+function filterRecords(records, category) {
+  var now = moment();
+  switch(category){
+    case assignmentDueTypes.OVERDUE:
+      return _.filter(records, function (record) {
+        return moment(record.getCellValue("Due")).isBefore(now, 'day');
+      });
+    case assignmentDueTypes.TODAY:
+      return _.filter(records, function(record){
+        return moment(record.getCellValue("Due")).isSame(moment(), 'day');
+      });
+    case assignmentDueTypes.UPCOMING:
+      return _.filter(records, function(record){
+        return moment(record.getCellValue("Due")).isAfter(moment(), 'day');
+      });
+    default:
+      console.error("no type for " + category);
+      return null;
+  }
 }
 
 export class ShowAssignments extends React.Component {
@@ -135,8 +109,10 @@ export class ShowAssignments extends React.Component {
     this.state = {
       showOverdue: true,
       showDueToday: true,
+      showUpcoming: true,
       showOverdueList: false,
       showTodayList: false,
+      showUpcomingList: false,
       showIndividualAssignment: false,
       selectedAssignment: null
     };
@@ -148,10 +124,13 @@ export class ShowAssignments extends React.Component {
     return (
       <>
       {this.state.showOverdue ? (
-        <OverDueAssignments records={this.props.assignmentRecords} onClick={this.showHideAssignment} toggleShow={this.toggleShow} showHide={this.state.showOverdueList}/>
+        <OverDueAssignments records={this.props.assignmentRecords} onClick={this.showHideAssignment} toggleShow={this.toggleShow} showHide={this.state.showOverdueList} assignmentDue={assignmentDueTypes.OVERDUE}/>
       ) : (<></>)}
       {this.state.showDueToday ? (
-        <DueTodayAssignments records={this.props.assignmentRecords} onClick={this.showHideAssignment} toggleShow={this.toggleShow} showHide={this.state.showTodayList}/>
+        <OverDueAssignments records={this.props.assignmentRecords} onClick={this.showHideAssignment} toggleShow={this.toggleShow} showHide={this.state.showTodayList} assignmentDue={assignmentDueTypes.TODAY}/>
+      ) : (<></>)}
+      {this.state.showUpcoming ? (
+        <OverDueAssignments records={this.props.assignmentRecords} onClick={this.showHideAssignment} toggleShow={this.toggleShow} showHide={this.state.showUpcomingList} assignmentDue={assignmentDueTypes.UPCOMING}/>
       ) : (<></>)}
       {this.state.showIndividualAssignment ? (
         <ShowIndividualAssignment record={this.state.selectedAssignment} onClick={this.showHideAssignment}/>
@@ -165,12 +144,14 @@ export class ShowAssignments extends React.Component {
     if(!this.state.showIndividualAssignment){
       this.setState({'showOverdue': false});
       this.setState({'showDueToday': false});
+      this.setState({'showUpcoming': false});
       this.setState({'showIndividualAssignment': true});
       this.setState({'selectedAssignment': record})
     }
     else{
       this.setState({'showOverdue': true});
       this.setState({'showDueToday': true});
+      this.setState({'showUpcoming': false});
       this.setState({'showIndividualAssignment': false});
       this.setState({'selectedAssignment': null})
     }
@@ -178,11 +159,17 @@ export class ShowAssignments extends React.Component {
 
   toggleShow(type){
     switch(type){
-      case 'today':
+      case assignmentDueTypes.TODAY:
         this.setState({'showTodayList': !this.state.showTodayList});
         break;
-      case 'overdue':
+      case assignmentDueTypes.OVERDUE:
         this.setState({'showOverdueList': !this.state.showOverdueList});
+        break;
+      case assignmentDueTypes.UPCOMING:
+        this.setState({'showUpcomingList': !this.state.showUpcomingList});
+        break;
+      default:
+        console.error("No type for " + type);
         break;
     }
   }
